b6e3f3d87f83bd7d0587688acf9502fd48bbd1f8
BabyMuu <16306402qq.com>
2022-06-05 19:20:20 +0800

    feature: 引入初始化CSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 引入初始化CSS
+import '@/assets/css/init_css.css'
 
 createApp(App).use(store).use(router).mount('#app')
